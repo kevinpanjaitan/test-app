e4ecb76f29c80037348cb5618756fc671f8e85d0
kevin <kevin.panjaitan@students.amikom.ac.id>
2017-09-29 10:39:28 +0700

    Splash Activity

---
+++ test-app/app/src/main/java/amikom/kevin/com/test_app/SplashActivity.java
@@ -0,0 +1,13 @@
+package amikom.kevin.com.test_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
