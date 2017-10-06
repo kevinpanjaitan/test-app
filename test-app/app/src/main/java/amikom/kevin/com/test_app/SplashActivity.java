10cc8689a2c4069090606eed7cab6da108edbe51
kevin <kevin.panjaitan@students.amikom.ac.id>
2017-10-06 10:26:44 +0700

    splash screen

--- test-app/app/src/main/java/amikom/kevin/com/test_app/SplashActivity.java
+++ test-app/app/src/main/java/amikom/kevin/com/test_app/SplashActivity.java
@@ -1,5 +1,6 @@
 package amikom.kevin.com.test_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,18 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
     }
 }
